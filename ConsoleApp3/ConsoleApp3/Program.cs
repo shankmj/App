780804870a49b09f188f41b7391e30e0625d6127
shankmj <40147484+shankmj@users.noreply.github.com>
2018-08-02 13:01:48 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderName = @"D:\ADC\ADC-Demo-Main\Pipeline_Configuration_File";
+            foreach (var files in System.IO.Directory.GetFiles(folderName))
+{
+                System.IO.FileInfo info = new System.IO.FileInfo(files);
+                var fileName = System.IO.Path.GetFileName(info.FullName);
+                Console.WriteLine(fileName);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
